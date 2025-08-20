17c934b58812a2aebadb1bf5fa4e65da9f92d638
piyush sachdeva <piyush.sachdeva2000@gmail.com>
2025-08-20 19:51:53 +0530

    New mod

--- Coding Tracker 0793/Program.cs
+++ Coding Tracker 0793/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+<<<<<<< HEAD
 Console.WriteLine("Hello, Amit tours and Travels! one more modification.");
+=======
+Console.WriteLine("Hello, Amit tours and Travels! one more modification");
+Console.WriteLine("Hello, Amit tours and Travels! one more modification");
+Console.WriteLine("Hello, Amit tours and Travels! one more modification....");
+
+>>>>>>> parent of f499b97 (Keeping one message)
